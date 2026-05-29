c643308124846433b97e10a16bc7b04fcd19e560
VasilUzn <hahacliped@gmail.com>
2026-05-29 21:41:42 +0300

    upgrade

--- Vasil_Uzunov/site.js
+++ Vasil_Uzunov/site.js
@@ -1,9 +1,13 @@
 function presmetniCena() {
   var torta = parseInt(document.getElementById("kol-torta").value) || 0;
   var biskviti = parseInt(document.getElementById("kol-biskviti").value) || 0;
+  var sladoled = parseInt(document.getElementById("kol-sladoled").value) || 0;
+  var sufle = parseInt(document.getElementById("kol-sufle").value) || 0;
   var cenaTorta = 12.00;
   var cenaBiskviti = 1.80;
-  var obshto = (torta * cenaTorta) + (biskviti * cenaBiskviti);
+  var cenaSladoled = 4.99;
+  var cenaSufle = 6.00;
+  var obshto = (torta * cenaTorta) + (biskviti * cenaBiskviti) + (sladoled * cenaSladoled) + (sufle * cenaSufle);
   var rezultat = document.getElementById("rezultat");
 
   if (obshto === 0) {
